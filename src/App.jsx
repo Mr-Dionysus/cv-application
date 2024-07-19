eb02d20919ffdfa49071a3cc01166243bd2bccc2
Denis <dbreznikov@gmail.com>
2024-07-20 01:18:58 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
     return (
         <>
             <h1>Hello, World!</h1>
+            <a name="" id="" className="btn btn-primary" href="#" role="button">
+                Button
+            </a>
         </>
     );
 }
